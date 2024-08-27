9d68895d79ec48a41631cdbc4134f84a23c78a5a
박상용 <psyok7@naver.com>
2024-08-27 14:11:10 +0900

    Feat: [FE] 재생성

---
+++ frontend/sel-frontend/src/components/campaign/Campaign.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import styles from './Navbar.module.css';
+
+const Campaign = () => {
+  return (
+    <div>
+      캠페인 페이지
+    </div>
+  );
+};
+
+export default Campaign;
