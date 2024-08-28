ca6d95b301b79f0e9d3fe121b5c6ab2941ada31a
heajun <jujng@naver.com>
2024-08-28 17:41:35 +0900

    Feat: mypage slide

--- frontend/sel-frontend/src/components/mypoint/MyPointHeader.jsx
+++ frontend/sel-frontend/src/components/mypoint/MyPointHeader.jsx
@@ -6,7 +6,7 @@ import styles from './MyPointHeader.module.css';
 const MyPointHeader = () => {
 
   const navigate = useNavigate();
-
+  const points = 1251;
   const handleViewPoints = () => {
     navigate('/mypoint');
   };
@@ -17,10 +17,10 @@ const MyPointHeader = () => {
   return (
     <div className={styles.headerBottom}>
       <div className={styles.userInfoBox}>
-        <div className={styles.userName}>홍길동님</div>
-        <div className={styles.pointAmount}>
-          1,000 <span>포인트</span>
-        </div>
+      <div className={styles.pointInfo}>
+        <p className={styles.pointLabel}>총 사용 가능 포인트</p>
+        <p className={styles.pointAmount}>{points.toLocaleString()} 포인트</p>
+      </div>
         <div className={styles.buttonGroup}>
           <button className={styles.historyButton} onClick={handleViewPoints}>내역보기</button>
           <button className={styles.chargeButton} onClick={handleUsePoints}>사용하기</button>
