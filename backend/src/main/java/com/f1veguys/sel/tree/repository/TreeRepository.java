2cc6bb4c274aaff6011b21bacce689f9cb922c3d
yeop <164111972+doongyeop@users.noreply.github.com>
2024-08-28 16:50:10 +0900

    Refactor: [BE] Tree Service revision

--- backend/src/main/java/com/f1veguys/sel/tree/repository/TreeRepository.java
+++ backend/src/main/java/com/f1veguys/sel/tree/repository/TreeRepository.java
@@ -1,13 +1,12 @@
 package com.f1veguys.sel.tree.repository;
 
 import com.f1veguys.sel.tree.domain.Tree;
-import com.f1veguys.sel.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface TreeRepository extends JpaRepository<Tree, Integer> {
 
-    Optional<Tree> findByUser_Id(Integer userId);
+    Optional<Tree> findByUser_Id(int userId);
 
 }
