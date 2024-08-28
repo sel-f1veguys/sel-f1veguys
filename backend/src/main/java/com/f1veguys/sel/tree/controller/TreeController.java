d54f33c9d2b633b51510d050e98951b20c96ede4
dongyeop <leedy977@gmail.com>
2024-08-29 01:23:23 +0900

    Chore: [BE] TreeController Response변경

--- backend/src/main/java/com/f1veguys/sel/tree/controller/TreeController.java
+++ backend/src/main/java/com/f1veguys/sel/tree/controller/TreeController.java
@@ -5,6 +5,7 @@ import com.f1veguys.sel.tree.service.TreeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,8 +18,9 @@ public class TreeController {
 
     @GetMapping("/{userId}")
     @Operation(summary = "나무 정보 조회", description = "나무의 상세 정보를 조회합니다.")
-    public Tree getTree(@PathVariable int userId) {
-        return treeService.getTree(userId);
+    public ResponseEntity<Tree> getTree(@PathVariable int userId) {
+        Tree tree = treeService.getTree(userId);
+        return ResponseEntity.ok(tree);
     }
 
 //    // 트리 시작
@@ -30,8 +32,9 @@ public class TreeController {
     // 물주기
     @PutMapping("/{id}/water")
     @Operation(summary = "나무에 물주기", description = "나무에 물(포인트, 500)를 줍니다.")
-    public Tree waterTree(@PathVariable int id) {
-        return treeService.waterTree(id);
+    public ResponseEntity<Tree> waterTree(@PathVariable int id) {
+        Tree tree = treeService.waterTree(id);
+        return ResponseEntity.ok(tree);
     }
 
 //    // 트리 종료 (id로 찾아서 종료)
@@ -43,8 +46,9 @@ public class TreeController {
     // 기프티콘 받기
     @PutMapping("/{id}/gifticon")
     @Operation(summary = "완성된 나무에서 기프티콘 받기", description = "완성된 나무에서 기프티콘 받기 버튼을 눌러 나무를 초기화합니다")
-    public Tree getGift(@PathVariable int id) {
-        return treeService.getGift(id);
+    public ResponseEntity<Tree> getGift(@PathVariable int id) {
+        Tree tree = treeService.waterTree(id);
+        return ResponseEntity.ok(tree);
     }
     
 }
