8f7c255dea00588d68bbc00b2e2fbc6bfea01e8d
dongyeop <leedy977@gmail.com>
2024-08-29 02:09:43 +0900

    Chore: [BE] Annotation change

--- backend/src/main/java/com/f1veguys/sel/domain/points/repository/PointsRepository.java
+++ backend/src/main/java/com/f1veguys/sel/domain/points/repository/PointsRepository.java
@@ -2,9 +2,11 @@ package com.f1veguys.sel.domain.points.repository;
 
 import com.f1veguys.sel.domain.points.domain.Points;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface PointsRepository extends JpaRepository<Points, Integer> {
     Optional<Points> findByUserId(int userId);
 }
