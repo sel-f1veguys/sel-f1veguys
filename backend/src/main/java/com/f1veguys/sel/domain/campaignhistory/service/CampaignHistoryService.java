2015f12ccd3b9c0f27214421b740161c300aaa05
dongyeop <ledy97@naver.com>
2024-08-31 15:37:21 +0900

    Init: Main Branch Init

--- backend/src/main/java/com/f1veguys/sel/domain/campaignhistory/service/CampaignHistoryService.java
+++
@@ -1,7 +0,0 @@
-package com.f1veguys.sel.domain.campaignhistory.service;
-
-import com.f1veguys.sel.domain.campaignhistory.domain.CampaignHistory;
-
-public interface CampaignHistoryService {
-    CampaignHistory participateInCampaign(int campaignId, int userId, int contributionAmount);
-}
